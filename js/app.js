9dc30153e48aaf9217ce789f355bcbeb4343fc45
Janinenw <janine.wexler@gmail.com>
2023-01-28 14:20:53 -0500

    multiplication algo

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-console.log("hello world")
+//Needed
+//Game title
+//Intructions- preferably not alert prompt, but can be
+//
+// sprite class if using sprites
+//- start game
+//- Generate multiplication problem
+// Timer for computer
+// check accuracy
+//progress bars
+// check win condition
+// 
+
+//
+// Will log random two random numbers 1-12 to console, multiply them, and display answer in console
+const newQuestion = () => {
+    num1 = Math.floor(Math.random() * 12);
+    num2= Math.floor(Math.random() * 12);
+    (`${num1} x ${num2} = ${(num1) * (num2)}`)
+}
+newQuestion()
+console.log(num1)
+console.log(num2)
+console.log((num1) * (num2))
+
