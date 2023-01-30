13b010654b21de6602085d85e127e5fea3099785
Janinenw <janine.wexler@gmail.com>
2023-01-29 20:51:47 -0500

    drop-down menu function

--- js/app.js
+++ js/app.js
@@ -14,15 +14,34 @@
 // check win condition
 // 
 
-//
-// Will log random two random numbers 1-12 to console, multiply them, and display answer in console
-const newQuestion = () => {
-    num1 = Math.floor(Math.random() * 12);
-    num2= Math.floor(Math.random() * 12);
-    (`${num1} x ${num2} = ${(num1) * (num2)}`)
-}
-newQuestion()
-console.log(num1)
-console.log(num2)
-console.log((num1) * (num2))
+// //
+// // Will log random two random numbers 1-12 to console, multiply them, and display answer in console
+// const newQuestion = () => {
+//     num1 = Math.floor(Math.random() * 12);
+//     num2= Math.floor(Math.random() * 12);
+//     (`${num1} x ${num2} = ${(num1) * (num2)}`)
+// }
+// newQuestion()
+// console.log(num1)
+// console.log(num2)
+// console.log((num1) * (num2))
 
+// Will log random two random numbers 1-12 to console, multiply them, and display answer in console
+// const newQuestion = () => {
+//     num1 = Math.floor(Math.random() * 12);
+//     num2= Math.floor(Math.random() * 12);
+//     (`${num1} x ${num2} = ${(num1) * (num2)}`)
+// }
+// newQuestion()
+// console.log(num1)
+// console.log(num2)
+// console.log((num1) * (num2))
+
+function showInstructions(){
+    const instructions = document.getElementById("instructions");
+    if(instructions.style.display === "none"){
+        instructions.style.display = "block";
+    }else{
+        instructions.style.display = "none";
+    }
+}
