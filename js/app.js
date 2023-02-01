7c784457f2fe24d3cbf938ff9b96d7c0aa5d06f4
Janinenw <janine.wexler@gmail.com>
2023-02-01 17:49:10 -0500

    game play function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-
+  
 document.addEventListener("DOMContentLoaded", function() {
   class Hero {
   constructor(name) {
@@ -23,10 +23,8 @@ document.addEventListener("DOMContentLoaded", function() {
   startButton.addEventListener("click", function() {
   const instructions = document.getElementById("instructions");
   instructions.classList.remove("hidden");
-
   const form = document.getElementsByTagName("form")[0];
-  form.classList.remove("hidden");
-
+  form.classList.remove("hidden")
   const question = document.getElementById("question");
   question.classList.remove("hidden");
 
@@ -34,6 +32,7 @@ document.addEventListener("DOMContentLoaded", function() {
   answer.classList.remove("hidden");
 
   const submitButton = document.getElementById("submit-button");
+  submitButton.addEventListener("click", checkAnswer);
   submitButton.classList.remove("hidden");
 
   const result = document.getElementById("result");
@@ -44,8 +43,38 @@ document.addEventListener("DOMContentLoaded", function() {
 
   const villainBar = document.getElementById("villain-bar");
   villainBar.classList.remove("hidden");
+
+  startButton.style.display = "none";
+  setTimeout(instructionsCheck, 5000);
 });
+}
+
+function instructionsCheck() {
+const userResponse = prompt("Have you read the instructions? Type 'yes' to continue.");
+if (userResponse === "yes") {
+   playGame()
+} else {
+  playGame()
+}
 }
 
 setup();
 });
+
+const playGame = () => {
+  const question = Math.floor(Math.random() * 12) + 1;
+  document.getElementById("question").innerHTML = `${question} x ${question}`;
+}
+
+const checkAnswer = (e) => {
+  e.preventDefault();
+  const answer = document.getElementById("answer").value;
+  const question = document.getElementById("question").innerHTML;
+  const response = question.split(" x ")[0] * question.split(" x ")[1];
+  if (answer == response) {
+  document.getElementById("result").innerHTML = "Correct!";
+  } else {
+  document.getElementById("result").innerHTML = "Try again.";
+  }
+  playGame();
+  }
